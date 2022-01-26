8e23199e2e926e41329cf039f49104d2fa93d694
alkmnd <asfsdg>
2022-01-26 16:46:06 +0300

    Initial Commit

---
+++ nabelova_1PW7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nabelova_1PW7
+//
+//  Created by Наталья Белова on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
